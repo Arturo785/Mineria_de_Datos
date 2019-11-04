b42800c89be7152705fdad79bc33ad40c4712b6c
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2019-11-04 12:57:20 -0600

    First push

---
+++ Proyecto mineria de datos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_mineria_de_datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
